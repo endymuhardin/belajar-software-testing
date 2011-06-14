b69b96e4a5116cff88bf5e435bca189ee5fc4aab
Endy Muhardin <endy.muhardin@gmail.com>
2011-06-14 10:32:05 +0700

    class yang mau dites

---
+++ src/main/java/belajar/junit/Calculator.java
@@ -0,0 +1,15 @@
+package belajar.junit;
+
+public class Calculator {
+	public Integer tambah(Integer x, Integer y){
+		return 0;
+	}
+	
+	public Integer kurang(Integer x, Integer y){
+		return 0;
+	}
+	
+	public Integer kali(Integer x, Integer y){
+		return 0;
+	}
+}
