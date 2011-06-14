722cd409ca34ccebf60e98b620c183b7e8170d94
Endy Muhardin <endy.muhardin@gmail.com>
2011-06-14 10:43:16 +0700

    implement error handling supaya lulus tes abnormal

--- src/main/java/belajar/junit/Calculator.java
+++ src/main/java/belajar/junit/Calculator.java
@@ -2,6 +2,9 @@ package belajar.junit;
 
 public class Calculator {
 	public Integer tambah(Integer x, Integer y){
+		if(x == null || y == null) {
+			throw new IllegalArgumentException("Input tidak boleh null");
+		}
 		return x + y;
 	}
 	
