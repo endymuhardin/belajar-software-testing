bc97fc46d7f3c55b09f0e6f3b9b08fc69318f6b6
Endy Muhardin <endy.muhardin@gmail.com>
2011-10-13 17:37:26 +0700

    remove unused import

--- src/test/java/belajar/dbunit/UserDaoTestIT.java
+++ src/test/java/belajar/dbunit/UserDaoTestIT.java
@@ -3,7 +3,6 @@ package belajar.dbunit;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
